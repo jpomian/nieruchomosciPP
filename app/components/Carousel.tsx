9b46a09234e9ed72b1a6599ae8387ea17ac87ec5
krrdash <jpomianowski@outlook.com>
2025-02-23 13:58:23 +0100

    redir

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -74,7 +74,7 @@ export default function Carousel() {
   }
 
   return (
-    <div className="w-full px-4 sm:px-6 lg:px-8 py-12">
+    <div id='oferty' className="w-full px-4 sm:px-6 lg:px-8 py-12">
       <div className="max-w-7xl mx-auto">
         <h1 className="text-3xl font-bold text-blue-500 text-center mb-6">
           Nieruchomości na sprzedaż
