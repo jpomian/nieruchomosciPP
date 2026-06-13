cbb9f35bf3a85ed6950574f498203ef73d1c2741
jpomian <jpomianowski@outlook.com>
2026-06-13 12:28:47 +0200

    Deleted redundant tracker

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@ import Carousel from "./components/Carousel";
 import Flashcards from "./components/Flashcards";
 import Hero from "./components/Hero";
 import { LogoTicker } from "./components/LogoTicker";
-import { MatomoTracker } from "./components/MatomoTracker";
 import Navbar from "./components/Navbar";
 import Pricing from "./components/Pricing";
 
 export default function Home() {
   return (
     <>
-      <MatomoTracker />
       <Navbar />
       <Hero />
       <Flashcards />
